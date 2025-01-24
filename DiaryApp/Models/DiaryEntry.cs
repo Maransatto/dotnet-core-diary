1ea0c7d2714d4e248e535632f405651a1435d004
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-24 17:02:25 -0300

    feat: 20. Annotations and avoiding null

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -1,13 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DiaryApp.Models
 {
     public class DiaryEntry
     {
-        // the name of the unique identifier needs to be Id or we could add
-        // a notation [Key] at the top (similar to decorators in NestJS)
+        // [Key] implicit covered if the property name is Id or DiaryEntryId (in this case)
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime Created { get; set; }
+        [Required]
+        public string? Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now;
 
     }
 }
