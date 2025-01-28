cbd7d8261de86857ad458c842dce14f30d8374f6
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-28 13:52:33 -0300

    feat: 26. Creating our Database and Table - Understanding Migrations

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using DiaryApp.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DiaryApp.Data
@@ -8,5 +9,17 @@ namespace DiaryApp.Data
         {
 
         }
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        // Four steps to add a table
+        // 1. Create a Model Class
+        // 2. Add a DB Set
+        // 3. add-migration AddDiaryEntryTable
+        // 3.1 for vs code: 
+        //          dotnet ef migrations add AddDiaryEntryTable // create the migration
+        //          dotnet tool install --global dotnet-ef
+        // 4. update-database
+        //          dotnet ef database update
     }
 }
